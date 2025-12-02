e2c6562bdd1107a385a95ab4a0d0cd8cb710d69e
ppedvLukasK <lukask@ppedv.de>
2025-12-02 11:04:45 +0100

    M005

---
+++ M005_Rechner/Calc.cs
@@ -0,0 +1,12 @@
+﻿namespace M005_Rechner;
+
+public class Calc
+{
+	public double Add(double x, double y) => x + y;
+
+	public double Sub(double x, double y) => x - y;
+
+	public double Mult(double x, double y) => x * y;
+
+	public double Div(double x, double y) => x / y;
+}
---
+++ M005_Rechner_Tests/RechnerTests.cs
@@ -0,0 +1,27 @@
+﻿using M005_Rechner;
+
+namespace M005_Rechner_Tests;
+
+/// <summary>
+/// Fine Code Coverage
+/// 
+/// Optionen -> Fine Code Coverage -> Editor Line Highlighting -> ShowLineCoverageHighlighting
+/// </summary>
+public class RechnerTests
+{
+	[Fact]
+	public void Test1()
+	{
+		Calc c = new Calc();
+		double summe = c.Add(2, 3);
+		Assert.Equal(5, summe);
+	}
+
+	[Fact]
+	public void Test2()
+	{
+		Calc c = new Calc();
+		double diff = c.Sub(2, 3);
+		Assert.Equal(-1, diff);
+	}
+}
