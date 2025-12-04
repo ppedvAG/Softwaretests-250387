b140db5b316f57c0d4d4232665f3e2600412e186
ppedvLukasK <lukask@ppedv.de>
2025-12-04 09:43:44 +0100

    Update Program.cs

--- M008_Bankkonto/Program.cs
+++ M008_Bankkonto/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace M008_Bankkonto;
+﻿using Newtonsoft.Json;
+
+namespace M008_Bankkonto;
 
 internal class Program
 {
 	static void Main(string[] args)
 	{
-		
+		int[] zahlen = [1, 2, 3, 4, 5];
+		string json = JsonConvert.SerializeObject(zahlen);
+		File.WriteAllText("Zahlen.json", json);
 	}
 }
